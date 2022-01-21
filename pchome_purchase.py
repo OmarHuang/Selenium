1f526bcee010c983f254c63e8a35757d4bf8bbc5
Omar Huang <huanghauchen@gmail.com>
2022-01-21 22:26:28 +0800

    Add retry method and update function

--- pchome_purchase.py
+++ pchome_purchase.py
@@ -2,7 +2,6 @@
 
 import yaml
 
-from dataclasses import dataclass
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,13 +10,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 # Loading private information from yaml
 data = yaml.safe_load(open("secrets.yaml"))
 
-# Keep Chrome logins
+# Webdriver settings
 options = webdriver.ChromeOptions()
 options.add_argument(data["profile"])
 
-# Webdriver settings
 driver = webdriver.Chrome(options=options)
-driver = webdriver.Chrome()
+driver.set_page_load_timeout(100)
 driver.maximize_window()
 wait = WebDriverWait(driver, 10)
 
@@ -39,39 +37,57 @@ xpath = {
 }
 
 
-@dataclass
 class PcHome:
-    account: str = data["account"]
-    cvc: int = data["cvc"]
-    password: int = data["password"]
-    status: bool = True
-    url: str = data["url"]
+    account = data["account"]
+    cvc = data["cvc"]
+    max_retry = 10
+    password = data["password"]
+    retry = 1
+    url = data["url"]
 
     def connect(self):
         # Connect to PcHome website
         driver.get(self.url)
         print("Connect successful")
+        self.check_status()
 
     def check_status(self):
         # Check if product is available for purchase
-        status = find(xpath["add_product"]).text
-        if status == "加入購物車":
-            wait.until(located((By.XPATH, xpath["add_product"]))).click()
-            print("Add product to shopping car")
-            # Click the shopping car
-            shopping_car = wait.until(clickable((By.XPATH, xpath["shopping_car"])))
-            driver.execute_script("arguments[0].click()", shopping_car)
-        else:
-            print("Not for sale or sold out")
-            driver.close()
-        return self.status
+        while self.retry <= self.max_retry:
+            status = find(xpath["add_product"]).text
+            if status == "加入購物車":
+                self.add_product()
+                break
+            elif self.retry >= self.max_retry:
+                print("Product is not for sale or sold out")
+                driver.close()
+            else:
+                driver.refresh()
+                self.retry += 1
+
+    def add_product(self):
+        # Add product to shopping car
+        wait.until(located((By.XPATH, xpath["add_product"]))).click()
+        print("Add product to shopping car")
+
+        # Click the shopping car
+        shopping_car = wait.until(clickable((By.XPATH, xpath["shopping_car"])))
+        driver.execute_script("arguments[0].click()", shopping_car)
+        self.check_login()
+
+    def check_login(self):
+        try:
+            self.login()
+        except Exception as login_exception:
+            self.purchase()
+            print(login_exception, "You are already logins")
 
     def login(self):
-        # Login PChome account
         wait.until(located((By.XPATH, xpath["account"]))).send_keys(self.account)
         wait.until(located((By.XPATH, xpath["password"]))).send_keys(self.password)
         wait.until(clickable((By.XPATH, xpath["login"]))).click()
         print("Login successful")
+        self.purchase()
 
     def purchase(self):
         # Select credit card as payment
@@ -90,10 +106,6 @@ class PcHome:
 pc = PcHome()
 if __name__ == "__main__":
     try:
-        # TODO: Should separate these function call.
         pc.connect()
-        pc.check_status()
-        pc.login()
-        pc.purchase()
     except Exception as e:
         print(e)
